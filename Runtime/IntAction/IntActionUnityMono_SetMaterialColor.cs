d29a52227c389609698fce83abaf5796dab636cf
Eloi Stree <eloistree@gmail.com>
2025-03-23 21:17:06 +0100

    Save: 2025_03_23_21_17

---
+++ Runtime/IntAction/IntActionUnityMono_SetMaterialColor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Eloi.IntAction
+{
+    public class IntActionUnityMono_SetMaterialColor : MonoBehaviour { 
+    
+        public Material[] m_materials;
+        public void SetColor(Color setColor)
+        {
+            foreach (var item in m_materials)
+            {
+                if (item != null)
+                    item.color = setColor;
+            }
+        }
+    }
+}
---
+++ Runtime/IntAction/IntActionUnityMono_SetRendererColor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Eloi.IntAction
+{
+    public class IntActionUnityMono_SetRendererColor : MonoBehaviour
+    {
+        public Renderer[] m_renderers;
+        public void SetColor(Color setColor)
+        {
+            foreach (var item in m_renderers)
+            {
+                if (item != null && item.material!=null)
+                    item.material.color = setColor;
+            }
+        }
+    }
+}
