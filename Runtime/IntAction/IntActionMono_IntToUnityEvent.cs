3d445268aeae6a72cf2e78587b70130cf06e9584
Eloi Stree <eloistree@gmail.com>
2025-03-23 22:03:52 +0100

    Save: 2025_03_23_22_03

---
+++ Runtime/IntAction/IntActionMono_IntToUnityEvent.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Eloi.IntAction
+{
+    public class IntActionMono_IntToUnityEvent : MonoBehaviour, I_IntActionBroadcastListener
+    {
+        public IntToEventSet[] m_data = new IntToEventSet[] {
+            new IntToEventSet(){m_integer = 700, m_onInvoke = new UnityEvent()},
+          };
+        public void PushIn(int integerValue)
+        {
+            for (int i = 0; i < m_data.Length; i++)
+            {
+                if (m_data[i]!=null&&
+                    m_data[i].m_integer == integerValue)
+                {
+                    m_data[i].m_onInvoke.Invoke();
+                    return;
+                }
+            }
+        }
+        [System.Serializable]
+        public class IntToEventSet
+        {
+            public int m_integer=0;
+            public UnityEvent m_onInvoke= new UnityEvent();
+        }
+    }
+
+}
